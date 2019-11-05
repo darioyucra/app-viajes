bcc1732284308ddb24ad995087fc6af3ca1e6322
DarioYucra <yucradario95@gmail.com>
2019-11-04 21:01:30 -0300

    agrego el boton de menu al header

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,9 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Button from 'react-bootstrap/Button';
 import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import RegisterLoginBtn from './RegisterLoginBtn';
 
 import {
@@ -25,8 +28,10 @@ class Header extends Component  {
   render (){ 
     return (
     <header>
-      <nav id="menuPrincipal"> 
-        <OverlayTrigger
+      <nav id="menuPrincipal">  
+      <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <OverlayTrigger
         trigger="click"
         key={"bottom"}
         placement={"bottom"}
@@ -40,7 +45,20 @@ class Header extends Component  {
         }>
           <Button variant="secondary"><FontAwesomeIcon icon={faUser} /></Button>
         </OverlayTrigger>
-
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+      </Navbar>
 
       </nav>
       <h1><a href="index.html"><img src={logo1}/></a></h1>
